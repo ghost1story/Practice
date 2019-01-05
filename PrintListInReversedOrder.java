32887707f01e74d41ec7a0224a05357b9dbefb31
ghost1story <1ghoststories@gmail.com>
2019-01-05 21:33:26 +0800

    从尾到头打印链表

--- PrintListInReversedOrder.java
+++ PrintListInReversedOrder.java
@@ -18,10 +18,29 @@ public class PrintListInReversedOrder {
 
     private ArrayList<Integer> reverse2(ListNode listNode) {
         ArrayList<Integer> list = new ArrayList<>();
-        while (listNode != null) {
+        if (listNode != null) {
             list.addAll(reverse2(listNode.next));
             list.add(listNode.value);
         }
         return list;
+    }
+
+    public static void main(String[] args) {
+        ListNode l = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        ListNode l3 = new ListNode(7);
+        l.next = l2;
+        l2.next = l3;
+
+        System.out.println(new PrintListInReversedOrder().reverse(l));
+    }
+}
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
     }
 }
