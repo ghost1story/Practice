5a06c557956eb99db5bad42391388a448cd4de35
ghost1story <1ghoststories@gmail.com>
2019-01-08 23:50:44 +0800

    二叉树的下一个节点

--- NextNodeInBinaryTree.java
+++ NextNodeInBinaryTree.java
@@ -1,34 +1,61 @@
-/** 
- * 有left -> left
- * 无left，有right -> right
- * 都无，是left -> 上一个
- * 都无，是right -> 向上直到当前为left的上一个
+/**
+ * 有右子树 -> 右子树下最下层的左子节点
+ * 有右子树，右子树下无左子树 -> 右子节点
+ * 无右子树 -> 向上，直到当前节点为上一节点的左子节点，
+ * 返回该节点的上一个节点。若上一节点为空，则返回空
  */
 
 public class NextNodeInBinaryTree {
  
-    private TreeNOde next(TreeNode root) {
+    private TreeNode next(TreeNode root) {
         if (root == null) {
             return null;
         }
-        if (root.left != null && root.right != null) {
-            if (root.left != null) {
-                return root.left;
-            } else {
-                return root.rigth;
+        TreeNode res = null;
+        if (root.right != null) {
+            root = root.right;
+            while (root.left != null) {
+                root = root.left;
             }
-        }
-        if (root.parent != null) {
-            if (root.parent.left.val == root.val) {
-                return root.parent;
-            } else {
-                while (parent.left.val != next.val) {
-                    TreeNode parent = root.parent;
-                    TreeNode next = root;
-                    next = parent;
-                    parent = parent.parent;
-                }
+            res = root;
+        } else {
+            while (root.parent != null && root.parent.left != root) {
+                root = root.parent;
             }
+            res = root.parent;
         }
+        return res;
     }
+
+    public static void main(String[] args) {
+        TreeNode n = new TreeNode(3);
+        TreeNode n2 = new TreeNode(32);
+        TreeNode n3 = new TreeNode(8);
+        TreeNode n4 = new TreeNode(9);
+        // n.right = n2;
+        // n2.parent = n;
+        // n2.left = n3;
+        // n3.parent = n2;
+        n.left = n2;
+        n2.parent = n;
+        n2.right = n3;
+        n3.parent = n2;
+        n3.right = n4;
+        n4.parent = n3;
+
+        NextNodeInBinaryTree t = new NextNodeInBinaryTree();
+        System.out.println(t.next(n2).val);
+        System.out.println(t.next(n3).val);
+        System.out.println(t.next(n4).val);
+        System.out.println(t.next(n).val);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode parent;
+
+    public TreeNode(int val) { this.val = val; }
 }
