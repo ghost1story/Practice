c5ac32f3bf8c5522c4b6cae715bbf78c21e8a9c4
ghost1story <1ghoststories@gmail.com>
2019-01-21 21:08:48 +0800

    1的个数

--- NumberOf1.java
+++ NumberOf1.java
@@ -9,13 +9,26 @@ public class NumberOf1 {
         return count;
     }
 
+    private int get2(int n) {
+        int count = 0;
+        int flag = 1;
+        // flag 左移 32 位后变为 0
+        while (flag != 0) {
+            if ((n & flag) != 0) {
+                count++;
+            }
+            flag = flag << 1;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         NumberOf1 n = new NumberOf1();
-        System.out.println(n.get(7));
-        System.out.println(n.get(8));
-        System.out.println(n.get(0));
-        System.out.println(n.get(1));
-        System.out.println(n.get(0xFFFFFFFF));
-        System.out.println(n.get(0x7FFFFFFF));
+        System.out.println(n.get2(7));
+        System.out.println(n.get2(8));
+        System.out.println(n.get2(0));
+        System.out.println(n.get2(1));
+        System.out.println(n.get2(0xFFFFFFFF));
+        System.out.println(n.get2(0x7FFFFFFF));
     }
 }
