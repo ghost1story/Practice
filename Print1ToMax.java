12a01e74f27a1d043274fdf7f54bf2c01f614312
unknown <1ghoststories@gmail.com>
2019-01-23 09:37:43 +0800

    打印从1到n的最大数

--- Print1ToMax.java
+++ Print1ToMax.java
@@ -12,10 +12,41 @@ public class Print1ToMax {
         System.out.println(max);
     }
 
+    private void print1ToMax(int n) {
+        if (n < 1) {
+            return;
+        }
+        char[] c = new char[n];
+        print1ToMax(c, 0);
+    }
+
+    private void print1ToMax(char[] c, int index) {
+        if (index == c.length) {
+            print(c);
+            return;
+        }
+        for (int i = 0; i < 10; i++) {
+            c[index] = (char) (i + '0');
+            print1ToMax(c, index + 1);
+        }
+    }
+
+    private void print(char[] c) {
+        int index = 0;
+        while (index < c.length && c[index] == '0') {
+            index++;
+        }
+        while (index < c.length) {
+            System.out.print(c[index++]);
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Print1ToMax p = new Print1ToMax();
-        p.print(1);
-        p.print(5);
-        p.print(10);
+        p.print1ToMax(1);
+        p.print1ToMax(5);
+        p.print1ToMax(0);
+        p.print1ToMax(-1);
     }
 }
