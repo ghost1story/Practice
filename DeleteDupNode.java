315c5714c6942067d0973c844edbe21d2dac03f3
ghost1story <1ghoststories@gmail.com>
2019-01-25 21:06:33 +0800

    删除重复链表

--- DeleteDupNode.java
+++ DeleteDupNode.java
@@ -8,9 +8,38 @@ public class DeleteDupNode {
         if (head.val == next.val) {
             do {
                 next = next.next;
-            } while (head.val == next.val);
+            } while (next != null && head.val == next.val);
+            return deleteNode(next);
         } else {
             head.next = deleteNode(head.next);
+            return head;
         }
+    }
+
+    public static void main(String[] args) {
+        // ListNode l = new ListNode(5);
+        ListNode l2 = new ListNode(7);
+        ListNode l3 = new ListNode(7);
+        ListNode l4 = new ListNode(7);
+        // l.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+
+        DeleteDupNode d = new DeleteDupNode();
+        ListNode n = d.deleteNode(l2);
+        while (n != null) {
+            System.out.println(n.val);
+            n = n.next;
+        }
+    }
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
     }
 }
