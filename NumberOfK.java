11593aae2652ca06171f5124e835e17a76a1f9d4
unknown <1ghoststories@gmail.com>
2019-03-06 09:29:35 +0800

    数组中数字的个数

--- NumberOfK.java
+++ NumberOfK.java
@@ -1,6 +1,6 @@
 public class NumberOfK {
 
-    int get(int[] nums, int k) {
+    /* int get(int[] nums, int k) {
         if (nums == null) {
             return -1;
         }
@@ -10,10 +10,8 @@ public class NumberOfK {
         if (start == -1 || end == -1) {
             return -1;
         }
-        // System.out.println("start: " + start);
-        // System.out.println("end: " + end);
         return end - start + 1;
-    }
+    } */
 
     int getFirstResursively(int[] nums, int k, int start, int end) {
         if (start > end) {
@@ -21,7 +19,6 @@ public class NumberOfK {
         }
         int res = -1;
         int mid = (start + end) / 2;
-        // System.out.println("mid: " + mid);
         if (nums[mid] == k && (mid == 0 || nums[mid - 1] != k)) {
             res = mid;
         } else if (nums[mid] == k && nums[mid - 1] == k) {
@@ -40,7 +37,6 @@ public class NumberOfK {
         }
         int res = -1;
         int mid = (start + end) / 2;
-        // System.out.println("mid: " + mid);
         if (nums[mid] == k && (mid == nums.length - 1 || nums[mid + 1] != k)) {
             res = mid;
         } else if (nums[mid] == k && nums[mid + 1] == k) {
@@ -53,11 +49,37 @@ public class NumberOfK {
         return res;
     }
 
+    int get(int[] nums, int k) {
+        if (nums == null) {
+            return -1;
+        }
+        int first = getNum(nums, k);
+        int end = getNum(nums, k + 1);
+        return (first == nums.length || nums[first] != k) ? 0 : end - first;
+    }
+
+    int getNum(int[] nums, int k) {
+        int before = 0;
+        int after = nums.length - 1;
+        int mid = 0;
+        while (before < after) {
+            mid = (before + after) / 2;
+            if (nums[mid] == k && (mid == 0 || nums[mid - 1] != k)) {
+                return mid;
+            } else if ((nums[mid] == k && nums[mid - 1] == k) || nums[mid] > k) {
+                after = mid - 1;
+            } else if (nums[mid] < k) {
+                before = mid + 1;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         NumberOfK k = new NumberOfK();
-        int[] n = {1, 2, 3, 3, 3, 9};
+        int[] n = {1, 2, 3, 3, 3, 4};
         int[] n2 = {1, 2, 3, 3, 3, 3};
-        int[] n3 = {3, 3, 3, 3, 4, 7};
+        int[] n3 = {3, 3, 3, 3, 4, 5};
         int[] n4 = {3, 3, 3, 3};
         int[] n5 = {2};
         System.out.println(k.get(n, 1));
