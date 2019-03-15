8371b1bc5c94d585c6e76d7047634a32afff06e9
unknown <1ghoststories@gmail.com>
2019-03-15 14:52:27 +0800

    二叉树中第k个数

--- KthNodesInBTS.java
+++ KthNodesInBTS.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 public class KthNodesInBTS {
 
-    static KthNodesInBTS ins = new KthNodesInBTS();
+    int res = -1;
+    int count;
 
     int get(BinaryTree root, int k) {
         if (root == null || k <= 0) return -1;
         // ArrayList<Integer> list = new ArrayList<>();
         // inOrderTree(root, k);
         // return k > list.size() ? -1 : list.get(list.size() - k);
-        return inOrderTree(root, k);
+        inOrderTree(root, k);
+        return res;
     }
 
     void inOrderTree(BinaryTree root, ArrayList<Integer> list) {
@@ -19,16 +21,17 @@ public class KthNodesInBTS {
         inOrderTree(root.right, list);
     }
 
-    int inOrderTree(BinaryTree root, int k) {
-        if (root == null) return -1;
+    void inOrderTree(BinaryTree root, int k) {
+        // if (root == null) return;
+        // inOrderTree(root.left, k);
+        // k--;
+        // if (k == 0) res = root.value;
+        // inOrderTree(root.right, k);
+        if (root == null || count >= k) return;
         inOrderTree(root.left, k);
-        if (k == 1) {
-            return root.value;
-        } else {
-            k--;
-        }
+        count++;
+        if (count == k) res = root.value;
         inOrderTree(root.right, k);
-        return -1;
     }
 
     static void connectBT(BinaryTree root, BinaryTree left, BinaryTree right) {
@@ -36,8 +39,10 @@ public class KthNodesInBTS {
         root.right = right;
     }
 
-    static void isRight(BinaryTree root, int k, int res) {
-        System.out.println(ins.get(root, k) == res ? true : false);
+    static void isRight(BinaryTree root, int k, int expect) {
+        KthNodesInBTS ins = new KthNodesInBTS();
+        // System.out.println(ins.get(root, k));
+        System.out.println(ins.get(root, k) == expect ? true : false);
     }
 
     public static void main(String[] args) {
@@ -53,33 +58,33 @@ public class KthNodesInBTS {
         connectBT(b3, b6, b7);
 
         isRight(b1, 0, -1);
-        isRight(b1, 1, 9);
-        isRight(b1, 2, 7);
-        isRight(b1, 3, 6);
-        isRight(b1, 5, 4);
-        isRight(b1, 7, 1);
+        isRight(b1, 1, 1);
+        isRight(b1, 2, 3);
+        isRight(b1, 3, 4);
+        isRight(b1, 5, 6);
+        isRight(b1, 7, 9);
         isRight(b1, 8, -1);
 
-        BinaryTree b8 = new BinaryTree(6);
-        BinaryTree b9 = new BinaryTree(5);
-        BinaryTree b10 = new BinaryTree(4);
-        connectBT(b8, b9, null);
-        connectBT(b9, b10, null);
-        isRight(b8, 6, -1);
-        isRight(b8, 1, 6);
-        isRight(b8, 3, 4);
+        // BinaryTree b8 = new BinaryTree(6);
+        // BinaryTree b9 = new BinaryTree(5);
+        // BinaryTree b10 = new BinaryTree(4);
+        // connectBT(b8, b9, null);
+        // connectBT(b9, b10, null);
+        // isRight(b8, 6, -1);
+        // isRight(b8, 1, 6);
+        // isRight(b8, 3, 4);
 
-        BinaryTree c1 = new BinaryTree(7);
-        BinaryTree c2 = new BinaryTree(8);
-        BinaryTree c3 = new BinaryTree(9);
-        connectBT(c1, null, c2);
-        connectBT(c2, null, c3);
-        isRight(c1, 1, 9);
-        isRight(c1, 3, 7);
+        // BinaryTree c1 = new BinaryTree(7);
+        // BinaryTree c2 = new BinaryTree(8);
+        // BinaryTree c3 = new BinaryTree(9);
+        // connectBT(c1, null, c2);
+        // connectBT(c2, null, c3);
+        // isRight(c1, 1, 9);
+        // isRight(c1, 3, 7);
 
-        BinaryTree c4 = new BinaryTree(1);
-        isRight(c4, 1, 1);
-        isRight(c4, 2, -1);
+        // BinaryTree c4 = new BinaryTree(1);
+        // isRight(c4, 1, 1);
+        // isRight(c4, 2, -1);
     }
 }
 
