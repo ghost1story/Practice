b68f3ef9b97872d4b14edd4029adbc25445eb070
ghost1story <1ghoststories@gmail.com>
2019-03-18 23:37:57 +0800

    走斜线的数量

--- NumbersOfSlash.java
+++ NumbersOfSlash.java
@@ -6,8 +6,9 @@ public class NumbersOfSlash {
 
     int getRes(int x, int y, int k) {
         if (k < getLeastFoot(x, y)) return -1;
-        else if (k == getLeastFoot(x, y)) return Math.min(x, y);
-        else return Math.max(x, y);
+        else if (Math.abs(y - x) % 2 != 0) return k - 1;
+        else if ((k - x) % 2 == 0) return k;
+        else return k - 2;
     }
 
     public static void main(String[] args) {
@@ -15,5 +16,11 @@ public class NumbersOfSlash {
         System.out.println(t.getRes(2, 3, 4));
         System.out.println(t.getRes(7, 7, 9));
         System.out.println(t.getRes(1, 2, 1));
+
+        System.out.println(t.getRes(6, 8, 8));
+        System.out.println(t.getRes(6, 7, 10));
+        System.out.println(t.getRes(7, 6, 10));
+        System.out.println(t.getRes(6, 8, 9));
+        System.out.println(t.getRes(8, 6, 9));
     }
 }
