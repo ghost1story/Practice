f942b09d95183d575827c5e92696e463b11968a4
ghost1story <1ghoststories@gmail.com>
2019-03-27 22:46:53 +0800

    判断平衡二叉树

--- BalanceBinaryTree.java
+++ BalanceBinaryTree.java
@@ -1,10 +1,11 @@
 public class BalanceBinaryTree {
 
     boolean isBalance(BinaryTree root) {
+        if (root == null) return true;
         return Math.abs(getDeepth(root.left) - getDeepth(root.right)) < 2;
     }
 
     int getDeepth(BinaryTree root) {
-        return root == null ? 0 : Math.max(getDeepth(root.left), getDeepth(root.right));
+        return root == null ? 0 : Math.max(getDeepth(root.left), getDeepth(root.right)) + 1;
     }
 }
